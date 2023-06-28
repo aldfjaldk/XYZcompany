266fe117800ac099c6109abcaf1e38207f0c8ade
ankush050720 <79631473+ankush050720@users.noreply.github.com>
2023-06-29 04:53:45 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -24,7 +24,6 @@ app.use(express.json());
 app.use(morgan('dev'));
 app.use("/api/v1/auth", authRoute)
 app.use(express.static(join(__dirname, '..', 'Home')));
-app.use(express.static(join(__dirname, '..', 'Components')));
 // app.use(express.static("../Login"));
 
 app.get("/", function (req, res) {
