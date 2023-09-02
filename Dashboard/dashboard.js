6341b91eaec900046b20faf4dee8c3dad3ba1e34
Projjwal Raj <134057438+aldfjaldk@users.noreply.github.com>
2023-09-03 01:05:57 +0530

    Update dashboard.js

--- Dashboard/dashboard.js
+++ Dashboard/dashboard.js
@@ -16,23 +16,23 @@ function fetchAndUpdateDashboardData() {
         const amountToPayElement = document.getElementById('amountToPay');
         amountToPayElement.textContent = `$${dashboardData.amountToPay || 0}`;
 
-        const amountToReceiveElement = document.getElementById('amountToReceive');
-        amountToReceiveElement.textContent = `$${dashboardData.amountToReceive || 0}`;
+        // const amountToReceiveElement = document.getElementById('amountToReceive');
+        // amountToReceiveElement.textContent = `$${dashboardData.amountToReceive || 0}`;
 
         const currentBalanceElement = document.getElementById('currentBalance');
         currentBalanceElement.textContent = `Current balance: $${dashboardData.currentBalance || 0}`;
 
         const overdueElement = document.getElementById('overdue');
         overdueElement.textContent = `Over Due: $${dashboardData.overDue || 0}`;
-        
+
         const currentBalanceElement2 = document.getElementById('currentBalance2');
         currentBalanceElement2.textContent = `Current balance: $${dashboardData.currentBalance || 0}`;
 
         const overdueElement2 = document.getElementById('overdue2');
         overdueElement2.textContent = `Over Due: $${dashboardData.overDue || 0}`;
 
-        const expenseElement = document.getElementById('expenses');
-        expenseElement.textContent = `$${dashboardData.expenses || 0}`;
+        // const expenseElement = document.getElementById('expenses');
+        // expenseElement.textContent = `$${dashboardData.expenses || 0}`;
 
         const followerElement = document.getElementById('followers');
         const followersValue = dashboardData.followers ? `+${dashboardData.followers}` : '+0';
@@ -43,7 +43,7 @@ function fetchAndUpdateDashboardData() {
         projectElement.textContent = projectsValue;
 
         const recordElement = document.getElementById('records');
-        const recordsValue  = dashboardData.records ? `+${dashboardData.records}` : '+0';
+        const recordsValue = dashboardData.records ? `+${dashboardData.records}` : '+0';
         recordElement.textContent = recordsValue;
 
         // Update the graph data
@@ -63,3 +63,66 @@ function fetchAndUpdateDashboardData() {
 }
 
 fetchAndUpdateDashboardData();
+
+let yourExpenses = 0;
+async function getTotalExpenses() {
+  try {
+    const response = await fetch("http://localhost:8000/api/v1/auth/displayexpense");
+    const data = await response.json();
+    console.log("Expenses data:", data.expenses[0]);
+
+    (data.expenses).map((expense) => {
+      var checkemail = localStorage.getItem("email");
+      if (checkemail === expense.useremail) {
+        yourExpenses += Number(expense.amount);
+      }
+    });
+    console.log("Here is the total expenses: ", yourExpenses);
+  } catch (error) {
+    console.log("Error fetching expenses:", error);
+  }
+}
+getTotalExpenses().then(() => {
+  yourExpenses = '$' + yourExpenses;
+  document.getElementById('expenses').innerHTML = yourExpenses
+})
+
+let amountToBeReceived = 0;
+async function getSalesOrders() {
+  try {
+    const response = await fetch("http://localhost:8000/api/v1/auth/display-sales-order");
+    const data = await response.json();
+    var checkemail = localStorage.getItem("email");
+    (data.salesOrder).map((dc) => {
+      if (checkemail === dc.email) {
+        amountToBeReceived += Number(dc.grandTotal)
+      }
+    });
+  } catch (error) {
+    console.log("Error fetching SalesOrders:", error);
+  }
+}
+
+
+
+async function getpr() {
+  try {
+    const response = await fetch("http://localhost:8000/api/v1/auth/displaypr");
+    const data = await response.json();
+
+    (data.prs).map((pr) => {
+      var checkemail = localStorage.getItem("email");
+      if (checkemail === pr.useremail) {
+        amountToBeReceived -= Number(pr.amount);
+      }
+    });
+  } catch (error) {
+    console.log("Error fetching prs:", error);
+  }
+}
+getSalesOrders().then(() => {
+  getpr().then( ()=> {
+    amountToBeReceived = '$' + Math.floor(amountToBeReceived);
+    document.getElementById('amountToReceive').innerHTML = amountToBeReceived;
+  })
+})
